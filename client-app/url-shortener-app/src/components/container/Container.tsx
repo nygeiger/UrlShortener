b32455db5c273072766ee1f6f64cdd8fd26ce64f
Nyles Geiger <nygeiger@gmail.com>
2026-04-10 21:12:41 -0400

    Initial Front end logic & components (Initial design + submit link form). Added Axios and ability to post link address to database through backend server

---
+++ client-app/url-shortener-app/src/components/container/Container.tsx
@@ -0,0 +1,12 @@
+import FormContainer from "../form-container/FormContainer";
+
+interface IContainerProps {
+}
+
+export default function Container(props: IContainerProps) {
+    return (
+        <>
+            <FormContainer/>
+        </>
+    )
+}
---
+++ client-app/url-shortener-app/src/components/footer/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <div className="py-5 bg-slate-900 text-white text-base text-center">
+            Copyright &#169; URLShortener | Nyles Geiger
+        </div>
+    )
+}
